b9c5b55ce42a87acd2007b647e4ebcc2434dde96
wojasinho <wojtek.koszela@gmail.com>
2017-04-11 12:08:14 +0200

    Add commentary

--- Apars/Apar.py
+++ Apars/Apar.py
@@ -5,8 +5,10 @@ import re
 import sys
 import pprint
 
+# Defined architecture
 Arch='x86_64'
 
+# Definded interested names of Product  (Mostly it should be RHEL Servers v. X)
 product_interest = (
     'Red Hat Enterprise Linux',
     'Red Hat Enterprise Linux (v. 5 server)',
@@ -14,21 +16,24 @@ product_interest = (
     'Red Hat Enterprise Linux Server (v. 7)',
 )
 
+# Definded not interested names of Product, which we can skip
 product_not_interest = (
     'Oracle Java for Red Hat Enterprise Linux',
     'Red Hat Enterprise Linux Supplementary',
     'Red Hat Enterprise Linux Server Supplementary (v. 6)',
     'Red Hat Enterprise Linux Server Supplementary (v. 7)',
 )
-
-
+#Append paths for Files with APARS
 sys.path.append(('/home/Wojasinho/Programming/Apars/apars/Apars/'))
+sys.path.append(('/home/wojasinho/Projects/apars/Apars/'))
 
 # print(sys.path)
 
-f = open('adv_database.php?adv_id=65438')
-print (f.readline())
+file = open('adv_database.php?adv_id=65438')
 
-print (f)
-# print (f)
+while True:
+
+    line=file.readline()
+    line=file.lstrip()
+    True
 
