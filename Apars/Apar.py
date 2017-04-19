970d9c33890d057eb936f45d731a7e9251f10500
root <root@IBM.(none)>
2017-04-20 00:07:59 +0200

    It works!! Script can find package in interested products for defined architecture.

--- Apars/Apar.py
+++ Apars/Apar.py
@@ -6,18 +6,17 @@ import sys
 import pprint
 
 # Defined architecture
-arch='x86_64'
+arch = 'x86_64'
 
 # List of candidate
-candidate={}
+candidate = {}
+products = []
+
 
 # Definded interested names of Product  (Mostly it should be RHEL Servers v. X)
 products_interest = (
-    'Red Hat Enterprise Linux',
-    # 'Red Hat Enterprise Linux (v. 5 server)',
-    'Red Hat Enterprise Linux Server (v. 6)',
-    #'Red Hat Enterprise Linux Server (v. 7)',
-    'Red Hat Enterprise Linux Desktop (v. 6)',
+    'Red Hat Enterprise Linux Workstation Optional (v. 6)',
+    'Red Hat Enterprise Linux Workstation (v. 6)',
 )
 # Definded not interested names of Product, which we can skip
 products_not_interest = (
@@ -26,14 +25,14 @@ products_not_interest = (
     'Red Hat Enterprise Linux Server Supplementary (v. 6)',
     'Red Hat Enterprise Linux Server Supplementary (v. 7)',
 )
-#Append paths for Files with APARS
+# Append paths for Files with APARS
 sys.path.append(('/home/Wojasinho/Programming/Apars/apars/Apars/'))
 sys.path.append(('/home/wojasinho/Projects/apars/Apars/'))
 
 # print(sys.path)
 
 # Defined char which, you can skip in line
-bad = ('@','<','!','#','{','}',',','.',';',':')
+bad = ('@', '<', '!', '#', '{', '}', ',', '.', ';', ':')
 
 file = open('adv_database.php?adv_id=65438')
 
@@ -42,75 +41,77 @@ while True:
     line = line.lstrip()
 
     # Skip white line
-    if len(line)==0:
+    if len(line) == 0:
         continue
     # Skip first 'bad' char in line :
-    if line[0] in bad :
+    if line[0] in bad:
         continue
 
     # Find on raw text 'Synopsis' (concept of patch)
-    if line[0:8]=='Synopsis':
+    if line[0:8] == 'Synopsis':
         Synopsis = line
 
         continue
 
     # Find on raw text 'Advisory ID' (ex. RHSA-2016:0760-01)
     if 'Advisory ID:       ' in line:
-        Advisory_ID=line
-
+        Advisory_ID = line
 
         continue
 
     # Find on raw text interest Product (ex.Product    : Red Hat Enterprise Linux)
     if 'Product:       ' in line:
-        Product=line.split(':')[1].lstrip()
+        Product = line.split(':')[1].lstrip()
 
         continue
 
     # Find on raw text date
     if 'Issue date       :' in line:
-        date=line
+        date = line
         continue
 
     if 'Package List:' in line:
-        Package=line
-        while True:
+        Package = line
+        break
+
+while True:
+
+    # Var for next loop 'while'
+    trigger = True
 
     # Format lines (strip method for whitespace from left side and colon from right side)
+    line = file.readline()
+    line = line.lstrip().rstrip(':\n')
+
+    # # Skip white line
+    if len(line) == 0:
+        continue
+    # Skip first 'bad' char in line :
+    if line[0] in bad:
+        continue
+
+    if line in products_interest:
+        print(line)
+        products.append(line)
+
+        while trigger:
             line = file.readline()
             line = line.lstrip().rstrip(':\n')
-            if len(line) == 0:
+
+            if "Source" in line:
                 continue
 
+            if arch in line:
 
-            if line in products_interest:
-                print (line)
-            # for i in products_interest:
-            #
-            #     if i == line:
-            #         candidate[i] = line
-            #         # package=[]
-            #         while True:
-            #             line = file.readline()
-            #             line = line.lstrip().rstrip(':\n')
-            #
-            #             if arch in line:
-            #                 while len(line) is not 0:
-            #                     line = file.readline()
-            #                     line = line.lstrip()
-            #                     if (line.split('.')[-2] == arch) :
-            #                         print (line)
-            #                     if len(line)==0:
-            #                         False
-            #
+                while len(line) > 0:
 
-                        # else :
-                        #     continue
+                    line = file.readline()
+                    line = line.lstrip()
 
+                    if len(line) == 0:
+                         trigger= False
+                         break
 
-                                # else
-                                #     line = file.readline()
-                                #     line = line.lstrip()
+                    if line.split('.')[-2] == arch:
+                        print(line)
 
-    # else:
-    #     continue
